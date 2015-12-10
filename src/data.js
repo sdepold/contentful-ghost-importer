4b0e97cab4c2649c01e1f384c8176a045c78f234
Sascha Depold <sascha@depold.com>
2015-12-10 22:24:22 +0100

    [Added] Import assets and replace references in markdown

--- src/data.js
+++ src/data.js
@@ -1,5 +1,7 @@
 'use strict';
 
+import { importAsset } from './asset';
+
 export function importData (space, data, contentTypes) {
   return space.getEntries().then((entries) => {
     return Promise.all([
@@ -38,26 +40,59 @@ function importTags (entries, space, data, contentTypes) {
 
 function importPosts (entries, space, data, contentTypes) {
   return importEntities(...arguments, 'post', (post) => {
-    return {
-      sys: { id: post.slug },
-      fields: {
-        title: { 'en-US': post.title },
-        slug: { 'en-US': post.slug },
-        body: { 'en-US': post.markdown },
-        publishedAt: { 'en-US': post.published_at },
-        metaTitle: { 'en-US': post.meta_title },
-        metaDescription: { 'en-US': post.meta_description },
-        author: {
-          'en-US': {
-            sys: {
-              type: 'Link',
-              linkType: 'Entry',
-              id: findUserById(data.users, post.author_id).slug
+    return replaceImagesInPost(...arguments, post)
+      .then(function (post) {
+        return {
+          sys: { id: post.slug },
+          fields: {
+            title: { 'en-US': post.title },
+            slug: { 'en-US': post.slug },
+            body: { 'en-US': post.markdown },
+            publishedAt: { 'en-US': post.published_at },
+            metaTitle: { 'en-US': post.meta_title },
+            metaDescription: { 'en-US': post.meta_description },
+            author: {
+              'en-US': {
+                sys: {
+                  type: 'Link',
+                  linkType: 'Entry',
+                  id: findUserById(data.users, post.author_id).slug
+                }
+              }
             }
           }
-        }
+        };
+      });
+  });
+}
+
+function replaceImagesInPost (entries, space, data, contentTypes, post) {
+  let matches = post.markdown.match(/!\[.*?\]\(.+?\)/g) || [];
+
+  return Promise.all(matches.map((match, index) => {
+    let fragments = match.match(/!\[(.*)\]\((.*)\)/);
+    let title     = fragments[1] || `image-${index}`;
+    let url       = (
+      fragments[2].indexOf('http') === 0 ? fragments[2] : `${data.blogHost}${fragments[2]}`
+    );
+
+    return importAsset(space, post, title, url).then((asset) => {
+      return [fragments[2], asset];
+    }, (err) => {
+      console.log(err);
+      return [fragments[2], null];
+    });
+  })).then((assetMap) => {
+    assetMap.forEach((map) => {
+      let oldPath = map[0];
+      let asset   = map[1];
+
+      if (asset) {
+        post.markdown = post.markdown.replace(oldPath, asset.fields.file['en-US'].url);
       }
-    };
+    });
+
+    return post;
   });
 }
 
@@ -75,9 +110,13 @@ function importEntities (entries, space, entities, entityContentType, entityName
         return entity;
       }
 
-      return space
-        .createEntry(entityContentType, dataMapper(entityData))
-        .catch(console.log)
+      let mappedData = dataMapper(entityData);
+      let promise    = (mappedData instanceof Promise) ? mappedData : Promise.resolve(mappedData);
+
+      return promise
+        .then((mappedData) => {
+          return space.createEntry(entityContentType, mappedData);
+        })
         .then((entity) => {
           console.log(`- Created ${entityName} "${entityData.slug}"`);
 
