c7d9ad452394328efe859ddac63d25fc2d3cd7aa
Sascha Depold <sascha@depold.com>
2015-12-26 22:13:57 +0100

    Extract parts of the logic to blog-importer

--- src/helper.js
+++ src/helper.js
@@ -1,17 +1,8 @@
 'use strict';
 
-import * as contentful from 'contentful-management';
-
-export function getClient (argv) {
-  return contentful.createClient({
-    accessToken: argv.token,
-    host: argv.host
-  });
-}
-
 export function getGhostData (argv) {
-  var ghostDataPath = argv._[0];
-  var result = require(ghostDataPath).db[0].data;
+  let ghostDataPath = argv._[0];
+  let result = require(ghostDataPath).db[0].data;
 
   result.blogHost = argv.blogHost;
 
